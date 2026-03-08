98c3f40e5d8a2aa78a28d0c858fa5765aa6388bd
MrProgrammerMan <jonas.baugerud@gmail.com>
2026-03-08 14:26:52 +0100

    feat(agenix): Add agenix nixos module to common config

--- modules/nixosModules/common.nix
+++ modules/nixosModules/common.nix
@@ -1,8 +1,9 @@
-{ self, ... }: {
+{ self, inputs, ... }: {
   flake.nixosModules.common = { ... }: {
     imports = with self.nixosModules; [
       bootstrap
       secrets
+      inputs.agenix.nixosModules.default
     ];
   };
 }
