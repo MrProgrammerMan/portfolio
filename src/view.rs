bfde0d6f86897730c69ac422e68adf706a0abc78
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-03-28 20:51:09 +0100

    feat(footer): add footer and improve css responsiveness

--- src/view.rs
+++ src/view.rs
@@ -1,9 +1,9 @@
 use leptos::prelude::*;
 use leptos_router::{
-    components::{Route, Router, Routes},
+    components::{A, Route, Router, Routes},
     path,
 };
-use r#static::header::Header;
+use r#static::{footer::Footer, header::Header};
 
 mod r#static;
 
@@ -20,13 +20,36 @@ pub fn App() -> impl IntoView {
                     <Route path=path!("/contact") view=Contact />
                 </Routes>
             </main>
+            <Footer />
         </Router>
     }
 }
 
 #[component]
 fn Home() -> impl IntoView {
-    view! { "The home page" }
+    view! {
+        <section class="reverse">
+            <div>
+                <img src="portrait.jpg" alt="Portrait of me, Jonas Hazeland Baugerud." />
+            </div>
+            <div>
+                <h1>"Hello, I'm Jonas"</h1>
+                <p>
+                    "I study "
+                    <a href="https://www.oslomet.no/studier/tkd/dataingenior">
+                        "Software Engineering at OsloMet"
+                    </a>
+                    ", a course focusing on programming, mathematics, design philosophy and cooperation. Currently, I study full time, but I am always interested in job opportunities to the side."
+                </p>
+                <p>
+                    "This is my portfolio. Here you will be able to find projects I have completed in order to see how I work(once I finish "
+                    <a href="https://github.com/MrProgrammerMan/portfolio">"the site"</a>
+                    "). You can also find information about me as a person, and get in touch with me if you want. I recommend you start in the "
+                    <A href="portfolio">"portfolio"</A>"."
+                </p>
+            </div>
+        </section>
+    }
 }
 
 #[component]
--- src/view/static/header.rs
+++ src/view/static/header.rs
@@ -5,7 +5,14 @@ use leptos_router::components::A;
 pub fn Header() -> impl IntoView {
     view! {
         <header>
-            <Logo/>
+            <Logo />
+            <label for="menu-btn" class="menu-icon">
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+            <input type="checkbox" id="menu-btn" hidden />
+            <p>"Jonas_Baugerud@OsloMet:~$"</p>
             <nav>
                 <A href="/">"Home"</A>
                 <A href="/about">"About"</A>
@@ -20,18 +27,82 @@ pub fn Header() -> impl IntoView {
 pub fn Logo() -> impl IntoView {
     view! {
         <svg viewBox="0 -10 227 100" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="22" cy="45" r="10" fill="#7888FF" opacity="0.5"/>
-            <path d="M49,25 L83,45 L49,65" fill="none" stroke="#7888FF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M93,25 L127,45 L93,65" fill="none" stroke="#7888FF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-            <line x1="137" y1="35" x2="171" y2="35" stroke="#7888FF" stroke-width="4" stroke-linecap="round"/>
-            <line x1="137" y1="55" x2="171" y2="55" stroke="#7888FF" stroke-width="4" stroke-linecap="round"/>
-            <line x1="98" y1="17" x2="93" y2="3" stroke="#F74C00" stroke-width="2" stroke-linecap="round" opacity="1"/>
-            <line x1="110" y1="15" x2="110" y2="0" stroke="#F74C00" stroke-width="2" stroke-linecap="round" opacity="1"/>
-            <line x1="122" y1="17" x2="127" y2="3" stroke="#F74C00" stroke-width="2" stroke-linecap="round" opacity="1"/>
-            <circle cx="198" cy="45" r="17" fill="none" stroke="#16BBA0" stroke-width="2.5"/>
-            <circle cx="198" cy="45" r="11" fill="none" stroke="#16BBA0" stroke-width="1.5" opacity="0.5"/>
-            <circle cx="198" cy="45" r="5" fill="#16BBA0"/>
+            <circle cx="22" cy="45" r="10" fill="#7888FF" opacity="0.5" />
+            <path
+                d="M49,25 L83,45 L49,65"
+                fill="none"
+                stroke="#7888FF"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+            <path
+                d="M93,25 L127,45 L93,65"
+                fill="none"
+                stroke="#7888FF"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+            <line
+                x1="137"
+                y1="35"
+                x2="171"
+                y2="35"
+                stroke="#7888FF"
+                stroke-width="4"
+                stroke-linecap="round"
+            />
+            <line
+                x1="137"
+                y1="55"
+                x2="171"
+                y2="55"
+                stroke="#7888FF"
+                stroke-width="4"
+                stroke-linecap="round"
+            />
+            <line
+                x1="98"
+                y1="17"
+                x2="93"
+                y2="3"
+                stroke="#F74C00"
+                stroke-width="2"
+                stroke-linecap="round"
+                opacity="1"
+            />
+            <line
+                x1="110"
+                y1="15"
+                x2="110"
+                y2="0"
+                stroke="#F74C00"
+                stroke-width="2"
+                stroke-linecap="round"
+                opacity="1"
+            />
+            <line
+                x1="122"
+                y1="17"
+                x2="127"
+                y2="3"
+                stroke="#F74C00"
+                stroke-width="2"
+                stroke-linecap="round"
+                opacity="1"
+            />
+            <circle cx="198" cy="45" r="17" fill="none" stroke="#16BBA0" stroke-width="2.5" />
+            <circle
+                cx="198"
+                cy="45"
+                r="11"
+                fill="none"
+                stroke="#16BBA0"
+                stroke-width="1.5"
+                opacity="0.5"
+            />
+            <circle cx="198" cy="45" r="5" fill="#16BBA0" />
         </svg>
-        <p>Jonas Baugerud</p>
     }
 }
