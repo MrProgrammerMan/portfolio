43a07357c9e64acfdc12fb8c97a697bf483645a4
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-04-05 12:43:45 +0200

    feat(module): add nixos module

---
+++ modules/module.nix
@@ -0,0 +1,28 @@
+{ inputs, self, ... }: {
+  flake.nixosModules.default = { config, lib, pkgs, system, ... }:
+    let
+        cfg = config.services.portfolio;
+    in {
+      options.services.portfolio = {
+        enable = lib.mkEnableOption "portfolio";
+        package = lib.mkOption {
+          type = lib.types.package;
+          default = self.packages.${system}.default;
+          description = "The package to use for the portfolio.";
+        };
+      };
+      config = lib.mkIf cfg.enable {
+        systemd.services.portfolio = {
+          description = "portfolio";
+          wantedBy = [ "multi-user.target" ];
+          after = [ "network.target" ];
+
+          serviceConfig = {
+            ExecStart = lib.getExe cfg.package;
+            Restart = "on-failure";
+            DynamicUser = true;
+          };
+        };
+      };
+    };
+}
