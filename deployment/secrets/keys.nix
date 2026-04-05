ab035489e3e5c347250792522c1897757488bd93
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-04-05 18:21:03 +0200

    chore(undo): remove failed deployment files

--- deployment/secrets/keys.nix
+++ deployment/secrets/keys.nix
@@ -1,4 +1,3 @@
 [
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINXAPg3uAm+rjuPGGeuxrCDfPfl03z0NEOadSUJDdfVA" # CD
-  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMWY9VfpbkhfTssSr/e8FkR0JXEOZb3u7mcV1gTIx/qT root@production" # production
 ]
