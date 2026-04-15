d4676a72381ce98ba08db598be575179a39d65f0
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-04-15 19:38:53 +0200

    feat(login): use appstate

--- src/view/pages/login.rs
+++ src/view/pages/login.rs
@@ -1,11 +1,9 @@
+#[cfg(feature = "ssr")]
+use crate::state::AppState;
 use leptos::{prelude::*, server, task::spawn_local};
 use leptos_router::{NavigateOptions, hooks::use_navigate};
 #[cfg(feature = "ssr")]
-use openidconnect::{
-    ClientId, ClientSecret, CsrfToken, IssuerUrl, Nonce, PkceCodeChallenge, RedirectUrl, Scope,
-    core::{CoreAuthenticationFlow, CoreClient, CoreProviderMetadata},
-    reqwest,
-};
+use openidconnect::{CsrfToken, Nonce, PkceCodeChallenge, Scope, core::CoreAuthenticationFlow};
 #[cfg(feature = "ssr")]
 use tower_sessions::Session;
 
@@ -26,34 +24,12 @@ pub fn Login() -> impl IntoView {
 
 #[server]
 pub async fn login() -> Result<String, ServerFnError> {
-    let client_id = std::env::var("CLIENT_ID").expect("CLIENT_ID should be set");
-    let client_secret = std::env::var("CLIENT_SECRET").expect("CLIENT_SECRET should be set");
-
-    let http_client = reqwest::ClientBuilder::new()
-        .redirect(reqwest::redirect::Policy::none())
-        .build()
-        .expect("Reqwest client should build");
-
-    let issuer = IssuerUrl::new("https://accounts.google.com".to_string())
-        .map_err(|e| ServerFnError::new(e.to_string()))?;
-
-    let provider_metadata = CoreProviderMetadata::discover_async(issuer, &http_client)
-        .await
-        .map_err(|e| ServerFnError::new(e.to_string()))?;
-
-    let client = CoreClient::from_provider_metadata(
-        provider_metadata,
-        ClientId::new(client_id),
-        Some(ClientSecret::new(client_secret)),
-    )
-    .set_redirect_uri(
-        RedirectUrl::new("http://localhost:3000/auth/callback".to_string())
-            .map_err(|e| ServerFnError::new(e.to_string()))?,
-    );
+    let state = use_context::<AppState>().unwrap();
 
     let (pkce_challenge, pkce_verifier) = PkceCodeChallenge::new_random_sha256();
 
-    let (auth_url, csrf_token, nonce) = client
+    let (auth_url, csrf_token, nonce) = state
+        .oauth_client
         .authorize_url(
             CoreAuthenticationFlow::AuthorizationCode,
             CsrfToken::new_random,
