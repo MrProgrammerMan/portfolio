f7d4fc182ed80331b0a4f4260fc7fd4ff84132d9
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-04-28 10:22:14 +0200

    refactor(timestamp): use lib timestamp function

--- server/src/auth/jwt.rs
+++ server/src/auth/jwt.rs
@@ -1,14 +1,12 @@
-use std::time::{SystemTime, UNIX_EPOCH};
-
-use jsonwebtoken::{EncodingKey, Header, encode};
+use jsonwebtoken::{EncodingKey, Header, encode, get_current_timestamp};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Claims {
     pub sub: String,
     pub role: Role,
-    pub iss: usize,
-    pub exp: usize,
+    pub iss: u64,
+    pub exp: u64,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -16,26 +14,19 @@ pub enum Role {
     Superuser,
 }
 
-pub fn generate(key: &EncodingKey, role: Role, sub: String) -> Result<String, jsonwebtoken::errors::Error> {
-    let now = unix_timestamp() as usize;
+pub fn generate(
+    key: &EncodingKey,
+    role: Role,
+    sub: String,
+) -> Result<String, jsonwebtoken::errors::Error> {
+    let now = get_current_timestamp();
 
     let claims = Claims {
         sub,
         role,
         iss: now,
-        exp: now + 300
+        exp: now + 300,
     };
 
-    encode(
-        &Header::default(),
-        &claims,
-        key
-    )
-}
-
-fn unix_timestamp() -> u64 {
-    SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards")
-        .as_secs()
+    encode(&Header::default(), &claims, key)
 }
