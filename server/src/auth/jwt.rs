f860ecec504c4c4625a6d356d7d28819197e6b6a
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-04-28 09:45:02 +0200

    feat(jwt): generate token

--- server/src/auth/jwt.rs
+++ server/src/auth/jwt.rs
@@ -1,3 +1,6 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use jsonwebtoken::{EncodingKey, Header, encode};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -11,4 +14,28 @@ pub struct Claims {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum Role {
     Superuser,
+}
+
+pub fn generate(key: &EncodingKey, role: Role, sub: String) -> Result<String, jsonwebtoken::errors::Error> {
+    let now = unix_timestamp() as usize;
+
+    let claims = Claims {
+        sub,
+        role,
+        iss: now,
+        exp: now + 300
+    };
+
+    encode(
+        &Header::default(),
+        &claims,
+        key
+    )
+}
+
+fn unix_timestamp() -> u64 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards")
+        .as_secs()
 }
